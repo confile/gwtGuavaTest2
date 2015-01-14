c28998b1400144fdda20266b6a997d3c8ba5f580
Michael Gorski <info@michaelgorski.de>
2015-01-14 13:02:43 +0100

    init

---
+++ src/com/test/client/Some.java
@@ -0,0 +1,9 @@
+package com.test.client;
+
+public interface Some {
+
+  public static void test() {
+    
+  }
+  
+}
---
+++ src/com/test/client/gin/NameTokens.java
@@ -0,0 +1,77 @@
+package com.test.client.gin;
+
+
+public class NameTokens {
+
+	public static final String start = "/start";
+	public static final String error = "/error";
+	public static final String photoEditor = "/photoEditor";
+	public static final String sub1 = "/sub1";
+	public static final String sub2 = "/sub2";
+	public static final String sub3 = "/sub3";
+	public static final String share = "/share";
+	public static final String stickerSelect = "/stickerSelect";
+	public static final String test = "/test";
+	public static final String login = "/login";
+	public static final String signup = "/signup";
+	public static final String item = "/post";
+	public static final String setting = "/setting";
+
+	
+	
+	public static String getStart() {
+		return start;
+	}
+	
+	public static String getError() {
+		return error;
+	}
+
+	public static String getPhotoEditor() {
+		return photoEditor;
+	}
+
+	public static String getSub1() {
+		return sub1;
+	}
+	
+	public static String getSub2() {
+		return sub2;
+	}
+	
+	public static String getSub3() {
+		return sub3;
+	}
+
+	public static String getShare() {
+		return share;
+	}
+
+	public static String getStickerSelect() {
+		return stickerSelect;
+	}
+
+	public static String getTest() {
+		return test;
+	}
+
+	public static String getLogin() {
+		return login;
+	}
+
+	public static String getSignup() {
+		return signup;
+	}
+
+	public static String getItem() {
+		return item;
+	}
+
+	public static String getSetting() {
+		return setting;
+	}
+
+	 
+	
+
+}
